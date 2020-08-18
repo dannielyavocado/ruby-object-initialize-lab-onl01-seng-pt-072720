28610d37b7261a8eb652c4cfeca1e4e3e2e89e50
Danniely Santos <dannielyingrid@gmail.com>
2020-08-18 15:20:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog 
   def initialize(dog_name, dog_breed = "Mutt")
     @name = dog_name
-    @breed - dog_breed
+    @breed = dog_breed
   end
   
   def name=(dog_name, dog_breed = "Mutt")
